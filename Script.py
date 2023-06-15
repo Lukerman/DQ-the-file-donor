f85e92419285ef76449ed4d8e81b9dfe86b74655
swaggster <135361033+TGxLegion@users.noreply.github.com>
2023-06-15 22:31:45 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -195,9 +195,9 @@ Nᴀᴍᴇ - {}"""
   
   
  ╔════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╗ 
- ▫️<a href=https://t.me/xyz_botz> ᴄʜᴀɴɴᴇʟ </a> 
+ ▫️<a href=https://t.me/ccupdates12> ᴄʜᴀɴɴᴇʟ </a> 
   
- ▫️<a href=https://t.me/Elsasupportgp> sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ </a> 
+ ▫️<a href=https://t.me/cinema4587> sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ </a> 
  ╚════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╝</b>"""
 
     IMDB_TEMPLATE_TXT = """
